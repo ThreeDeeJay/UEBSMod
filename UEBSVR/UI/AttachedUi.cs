c3f0f6c63904ae7b84d737b87d2fb4f16714cff8
antispamtutoruser123 <parker.brian.e@gmail.com>
2023-09-05 17:05:41 +0300

    Add files via upload

---
+++ UEBSVR/UI/AttachedUi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace UEBSVR
+{
+    class AttachedUi : MonoBehaviour
+    {
+        private Transform targetTransform;
+
+        public static void Create<TAttachedUi>(Canvas canvas, float scale = 0)
+            where TAttachedUi : AttachedUi
+        {
+            var instance = canvas.gameObject.AddComponent<TAttachedUi>();
+            if (scale > 0) canvas.transform.localScale = new Vector3(scale, .0008f, scale); ;
+            canvas.renderMode = RenderMode.WorldSpace;
+            canvas.transform.Find("PRESSK").transform.localPosition = new Vector3(0,141.0943f,0);
+        }
+        protected virtual void Start()
+        {
+            transform.Rotate(0, 180f, 0);
+        }
+        protected virtual void Update()
+        {
+            UpdateTransform();
+        }
+
+        public void SetTargetTransform(Transform target)
+        {
+            targetTransform = target;
+        }
+
+        private void UpdateTransform()
+        {
+            transform.position = Camera.main.transform.position + Camera.main.transform.forward;
+            transform.rotation = Camera.main.transform.rotation;
+        }
+    }
+}
